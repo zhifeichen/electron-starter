ccb3bc3ae7a1352f8e2c3858d105c5ca68707ce3
zhifei.chen <zhifei.chen@csst.com>
2017-06-30 16:28:08 +0800

    first commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { Window, TitleBar, Text } from 'react-desktop/windows';
+
+const remote = require('electron').remote;
+
+export default class extends Component {
+  static defaultProps = {
+    color: '#cc7f29',
+    theme: 'light'
+  };
+
+  close() {
+    const win = remote.getCurrentWindow();
+    console.log(win);
+    win.close();
+  }
+
+  render() {
+    return (
+      <Window
+        color={this.props.color}
+        theme={this.props.theme}
+        chrome
+        height="100%"
+        padding="12px"
+      >
+        <TitleBar title="My Windows Application" controls onCloseClick={this.close}/>
+        <Text color={this.props.theme === 'dark' ? 'white' : '#333'}>Hello World</Text>
+      </Window>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+// import webpack from 'webpack';
+const path = require('path');
+
+module.exports = {
+  entry: [
+    path.join(__dirname, 'src/index.js'),
+  ],
+
+  target: 'electron-renderer',
+
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
+    ],
+  },
+
+  output: {
+    path: path.join(__dirname, 'src'),
+    filename: 'bundle.js',
+    // https://github.com/webpack/webpack/issues/1114
+    libraryTarget: 'commonjs2',
+  },
+};
