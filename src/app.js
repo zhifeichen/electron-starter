7efa75e55f921ed908d00bda1e67ced9f9cc34a5
zhifei.chen <zhifei.chen@csst.com>
2017-06-30 17:30:28 +0800

    feat: add window size manage.

--- src/app.js
+++ src/app.js
@@ -4,12 +4,39 @@ import { Window, TitleBar, Text } from 'react-desktop/windows';
 const remote = require('electron').remote;
 
 export default class extends Component {
-  constructor() {
-    super();
-    this.defaultProps = {
-      color: '#cc7f29',
-      theme: 'light',
-    };
+  static defaultProps = {
+    color: '#cc7f29',
+    theme: 'dark',
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {  isMaximized: false };
+  }
+
+  minimize() {
+    remote.getCurrentWindow().minimize();
+  }
+
+  maximize() {
+    remote.getCurrentWindow().maximize();
+  }
+
+  unmaximize() {
+    remote.getCurrentWindow().unmaximize();
+  }
+
+  toggleMaximize() {
+    this.setState({ isMaximized: !this.state.isMaximized });
+    if(this.state.isMaximized) {
+      this.maximize();
+    } else {
+      this.unmaximize();
+    }
+  }
+
+  restore() {
+    remote.getCurrentWindow().restore();
   }
 
   close() {
@@ -26,7 +53,14 @@ export default class extends Component {
         height="100%"
         padding="12px"
       >
-        <TitleBar title="My Windows Application" controls onCloseClick={this.close}/>
+        <TitleBar
+          title="My Windows Application"
+          controls
+          onCloseClick={this.close}
+          onMinimizeClick={this.minimize}
+          onMaximizeClick={this.toggleMaximize}
+          onRestoreDownClick={this.toggleMaximize}
+        />
         <Text color={this.props.theme === 'dark' ? 'white' : '#333'}>Hello World</Text>
       </Window>
     );
